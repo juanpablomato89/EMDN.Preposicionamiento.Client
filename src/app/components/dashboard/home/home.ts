09951fdbb467688eee7b9c6bde3c262edf4c9d9b
Juan Pablo <juanpablomato89@gmail.com>
2026-03-31 14:48:46 -0400

    actualizando el mapa de cuba en el componente home

--- src/app/components/dashboard/home/home.ts
+++ src/app/components/dashboard/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapaCubaOffline } from './mapa-cuba-offline/mapa-cuba-offline';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, MapaCubaOffline],
+  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
---
+++ src/app/components/dashboard/home/mapa-cuba-offline/mapa-cuba-offline.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-cuba-offline',
+  imports: [CommonModule],
+  standalone:true,
+  templateUrl: './mapa-cuba-offline.html',
+  styleUrl: './mapa-cuba-offline.css',
+})
+export class MapaCubaOffline {
+
+}
